256a2385b1a5de45a15f9c39ddef569f0cd71eb1
Lukian <lukian55d@gmail.com>
2026-03-03 19:27:38 +0000

    fixed flake8

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from blog.models import Commentary, User, Post
 from django.contrib.auth.models import Group
 
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ("title", "owner", "created_time")
@@ -9,12 +10,14 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ("created_time", "owner")
     ordering = ("-created_time",)
 
+
 @admin.register(Commentary)
 class CommentaryAdmin(admin.ModelAdmin):
     list_display = ("post", "user", "content", "created_time")
     search_fields = ("content", "user__username", "post__title")
     list_filter = ("created_time",)
     ordering = ("-created_time",)
+
 
 admin.site.register(User)
 admin.site.unregister(Group)
--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class User(AbstractUser):
     def __str__(self):
         return self.username
@@ -24,16 +25,16 @@ class Commentary(models.Model):
     user = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='comments',
+        related_name="comments",
     )
 
     post = models.ForeignKey(
         Post,
         on_delete=models.CASCADE,
-        related_name='comments',
+            related_name="comments",
     )
+
     class Meta:
-        ordering = ['-created_time']
+        ordering = ["-created_time"]
     created_time = models.DateTimeField(auto_now_add=True)
     content = models.TextField()
-
